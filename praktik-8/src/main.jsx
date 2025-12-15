ebc171723479e514363b8aad7a7592f1efedd1aa
alfnsyhri <alfinsyahri1025@gmail.com>
2025-12-16 01:48:06 +0700

    Update main.jsx

--- praktik-8/src/main.jsx
+++ praktik-8/src/main.jsx
@@ -1,10 +1,17 @@
+// main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <-- Import BrowserRouter di sini
 import './index.css'
 import App from './App.jsx'
 
+// Basename harus sama dengan nama folder/repositori Anda
+const BASE_PATH = "/praktik_web_modern_alfinsyahri/praktik-8"; 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename={BASE_PATH}> {/* <--- Router dan Basename di sini */}
+        <App />
+    </BrowserRouter>
   </StrictMode>,
 )
